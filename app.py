5d0774f99628aef11d0f0c129faf03b575faadce
ryun0suk3 <139580951+ryun0suk3@users.noreply.github.com>
2024-04-17 03:01:04 +0000

    port changes T-T

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, Response
+# from camera import VideoCamera
 
 app = Flask(__name__)
 
@@ -10,14 +11,17 @@ def home():
 def login():
   return render_template("login.html")
 
-# @app.route("/about")
-# def login():
-#   return render_template("about.html")
+# def gen(camera):
+#   while True:
+#     frame = camera.get_frame()
+#     yield (b'--frame\r\n'
+#            b'Content-Type: image/jpeg\r\n\r\n' + frame
+#            + b'\r\n\r\n')
 
-# @app.route("/contact")
-# def login():
-#   return render_template("contact.html")
-
+# @app.route('/video_feed')
+# def video_feed():
+#   return Response(gen(VideoCamera()),
+#                   mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
