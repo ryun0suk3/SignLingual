1136ce0730d2017038f5aa533b62e2c8e7ec40f7
Tanvi <tanvipatil245@gmail.com>
2024-04-22 00:41:57 +0530

    suggestio

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
 app.config['MYSQL_DB'] = 'user-system'
-app.config['MYSQL_PORT'] = 3308
+app.config['MYSQL_PORT'] = 3306
 
 mysql = MySQL(app)
 
